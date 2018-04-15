621f58aa9150c321fcfe9afce22b307c181067d6
George V <georgevuong18@gmail.com>
2018-04-14 17:50:36 -0700

    add code to exclude redux-logger from production, and add production plugins

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,11 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+    const { logger } = require('redux-logger');
+    middlewares.push(logger);
+}
 // configureStore is used to apply preloadedState and middleware
 const configureStore = () => (
-    createStore(rootReducer, applyMiddleware(thunk))
+    createStore(rootReducer, applyMiddleware(...middlewares))
 );
 
 export default configureStore;
